6f879f127ddf5fcc2d83bf79b78baef9c0601f6b
Kevin Liu <kevinliu5890@gmail.com>
2024-04-05 21:26:24 +0000

    final commit

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -5,4 +5,7 @@
 
 --select sum(bats) from players where first_name = "Barry" and last_name = "Bonds"
 --select id from players where first_name = "Barry" and last_name = "Bonds" => 1678
-select sum(stats.hits) from stats inner join players on players.id = stats.player_id where first_name = "Barry" and last_name = "Bonds"
+select sum(stats.hits) 
+from stats inner join players 
+on players.id = stats.player_id 
+where players.first_name = "Barry" and players.last_name = "Bonds"
